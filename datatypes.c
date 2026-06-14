094dad52f61766a4ab5600a6ff05c102fcd74cf2
Damisetty Tapan <damisettytapan@gmail.com>
2026-06-14 18:19:23 +0530

    Added data types example

---
+++ datatypes.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int marks = 95;
+    float percentage = 95.0;
+    char grade = 'A';
+    printf("Marks = %d\n", marks);
+    printf("Percentage = %.1f\n", percentage);
+    printf("Grade = %c\n", grade);
+    return 0;
+}
