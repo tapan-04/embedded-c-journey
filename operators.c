450a62a3a69f7bd69fd6f5ef3b3011fcccaaf31b
Damisetty Tapan <damisettytapan@gmail.com>
2026-06-16 11:39:48 +0530

    Learned modulus operator and even odd programs

---
+++ operators.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int num = 15;
+    if (num % 2 == 0)
+    {
+        printf("Even Number= %d\n", num);
+    }
+    else 
+    {
+        printf("Odd Number= %d\n", num);
+    }
+    return 0;
+}
