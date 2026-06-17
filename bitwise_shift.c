aa5d71f4582ffdb1181d3c11cbc04fe7da6c4071
Damisetty Tapan <damisettytapan@gmail.com>
2026-06-17 22:12:10 +0530

    Learned bitwise shift operators

---
+++ bitwise_shift.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("5 << 1 = %d\n", 5 << 1);
+    printf("5 << 2 = %d\n", 5 << 2);
+
+    printf("8 >> 1 = %d\n", 8 >> 1);
+    printf("16 >> 2 = %d\n", 16 >> 2);
+
+    return 0;
+}
