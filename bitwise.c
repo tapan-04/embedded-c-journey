1af9f76d2e40345844a1818d0c8862a292ffc9ec
Damisetty Tapan <damisettytapan@gmail.com>
2026-06-17 21:30:22 +0530

    Learned basic bitwise operators

---
+++ bitwise.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 5;
+    int b = 3;
+    
+    printf("AND = %d\n", a & b);
+    printf("OR = %d\n", a | b);
+    printf("xor = %d\n", a ^ b);
+
+    return 0;
+}
